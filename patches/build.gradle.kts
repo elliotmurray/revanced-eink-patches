92ea628cb9e0ec9b9a09dd2a8260eb1aceaae6f8
Elliot Murray <ells1231@gmail.com>
2024-12-22 15:55:49 +0000

    fix(setup): Initial setup

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -1,12 +1,12 @@
-group = "app.revanced"
+group = "uk.co.elliotmurray.revanced.eink"
 
 patches {
     about {
-        name = "ReVanced Patches template"
-        description = "Patches template for ReVanced"
-        source = "git@github.com:revanced/revanced-patches-template.git"
-        author = "ReVanced"
-        contact = "contact@revanced.app"
+        name = "ReVanced eink Patches"
+        description = "Patches for eink tablets"
+        source = "git@github.com:elliotmurray/revanced-eink-patches.git"
+        author = "Elliot Murray"
+        contact = "elliotmmurray@gmail.com"
         website = "https://revanced.app"
         license = "GNU General Public License v3.0"
     }
